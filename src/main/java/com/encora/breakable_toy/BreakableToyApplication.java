9dcd73f1fcfdd700db3beb1a7351fd0f519b2453
“Rafael <francisco.lezama@encora.com>
2024-09-03 18:54:30 -0600

    maven and springboot configuration

---
+++ src/main/java/com/encora/breakable_toy/BreakableToyApplication.java
@@ -0,0 +1,13 @@
+package com.encora.breakable_toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakableToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakableToyApplication.class, args);
+	}
+
+}
